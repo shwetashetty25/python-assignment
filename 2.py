365bac2f81935b126df63ccc5a088ebc6930c8eb
Shweta Shetty <shettyshweta48@gmail.com>
2025-01-17 19:28:10 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,4 @@
+#2Write a Python program that takes a user’s name and prints a greeting with that name in uppercase and lowercase.
+name = input("Enter your name: ")
+print(f"Hello, {name.upper()}!")
+print(f"Hello, {name.lower()}!")
